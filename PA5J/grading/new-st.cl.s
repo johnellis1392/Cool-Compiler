41be8ca09152708a082530ef9a9392f0c589f869
Compilers <compilers@compilers-vm.(none)>
2015-05-04 15:50:59 -0700

    Beginning PA5

---
+++ PA5J/grading/new-st.cl.s
@@ -0,0 +1,250 @@
+# start of generated code
+	.data
+	.align	2
+	.globl	class_nameTab
+	.globl	Main_protObj
+	.globl	Int_protObj
+	.globl	String_protObj
+	.globl	bool_const0
+	.globl	bool_const1
+	.globl	_int_tag
+	.globl	_bool_tag
+	.globl	_string_tag
+_int_tag:
+	.word	0
+_bool_tag:
+	.word	0
+_string_tag:
+	.word	0
+	.globl	_MemMgr_INITIALIZER
+_MemMgr_INITIALIZER:
+	.word	_NoGC_Init
+	.globl	_MemMgr_COLLECTOR
+_MemMgr_COLLECTOR:
+	.word	_NoGC_Collect
+	.globl	_MemMgr_TEST
+_MemMgr_TEST:
+	.word	0
+	.word	-1
+str_const14:
+	.word	0
+	.word	5
+	.word	
+	.word	int_const1
+	.byte	0	
+	.align	2
+	.word	-1
+str_const13:
+	.word	0
+	.word	6
+	.word	
+	.word	int_const2
+	.ascii	"Main"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const12:
+	.word	0
+	.word	5
+	.word	
+	.word	int_const0
+	.ascii	"A"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const11:
+	.word	0
+	.word	6
+	.word	
+	.word	int_const3
+	.ascii	"String"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const10:
+	.word	0
+	.word	6
+	.word	
+	.word	int_const2
+	.ascii	"Bool"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const9:
+	.word	0
+	.word	5
+	.word	
+	.word	int_const4
+	.ascii	"Int"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const8:
+	.word	0
+	.word	5
+	.word	
+	.word	int_const5
+	.ascii	"IO"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const7:
+	.word	0
+	.word	6
+	.word	
+	.word	int_const3
+	.ascii	"Object"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const6:
+	.word	0
+	.word	7
+	.word	
+	.word	int_const6
+	.ascii	"_prim_slot"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const5:
+	.word	0
+	.word	7
+	.word	
+	.word	int_const7
+	.ascii	"SELF_TYPE"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const4:
+	.word	0
+	.word	7
+	.word	
+	.word	int_const7
+	.ascii	"_no_class"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const3:
+	.word	0
+	.word	8
+	.word	
+	.word	int_const8
+	.ascii	"<basic class>"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const2:
+	.word	0
+	.word	5
+	.word	
+	.word	int_const0
+	.ascii	"\n"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const1:
+	.word	0
+	.word	6
+	.word	
+	.word	int_const9
+	.ascii	"x is "
+	.byte	0	
+	.align	2
+	.word	-1
+str_const0:
+	.word	0
+	.word	7
+	.word	
+	.word	int_const10
+	.ascii	"./new-st.cl"
+	.byte	0	
+	.align	2
+	.word	-1
+int_const10:
+	.word	0
+	.word	4
+	.word	
+	.word	11
+	.word	-1
+int_const9:
+	.word	0
+	.word	4
+	.word	
+	.word	5
+	.word	-1
+int_const8:
+	.word	0
+	.word	4
+	.word	
+	.word	13
+	.word	-1
+int_const7:
+	.word	0
+	.word	4
+	.word	
+	.word	9
+	.word	-1
+int_const6:
+	.word	0
+	.word	4
+	.word	
+	.word	10
+	.word	-1
+int_const5:
+	.word	0
+	.word	4
+	.word	
+	.word	2
+	.word	-1
+int_const4:
+	.word	0
+	.word	4
+	.word	
+	.word	3
+	.word	-1
+int_const3:
+	.word	0
+	.word	4
+	.word	
+	.word	6
+	.word	-1
+int_const2:
+	.word	0
+	.word	4
+	.word	
+	.word	4
+	.word	-1
+int_const1:
+	.word	0
+	.word	4
+	.word	
+	.word	0
+	.word	-1
+int_const0:
+	.word	0
+	.word	4
+	.word	
+	.word	1
+	.word	-1
+bool_const0:
+	.word	0
+	.word	4
+	.word	
+	.word	0
+	.word	-1
+bool_const1:
+	.word	0
+	.word	4
+	.word	
+	.word	1
+	.globl	heap_start
+heap_start:
+	.word	0
+	.text
+	.globl	Main_init
+	.globl	Int_init
+	.globl	String_init
+	.globl	Bool_init
+	.globl	Main.main
+
+# end of generated code
